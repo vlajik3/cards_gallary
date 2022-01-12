a17eab00f2295f3cae77d16bb30c5134e881e0c7
vlajik3 <celishchevva@yandex.ru>
2022-01-12 23:46:02 +0300

    small refactoring

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const slides = document.querySelectorAll('.slide')
+
+const slidesPlugin = (slideNumber = 2) => {
+    const slides = document.querySelectorAll('.slide')
+
+    slides[slideNumber].classList.add('active')
 
 for(const slide of slides) {
     slide.addEventListener('click', () => {
@@ -13,3 +17,6 @@ function clearActiveClasses() {
         slide.classList.remove('active')
     })
 }
+}
+
+slidesPlugin()
